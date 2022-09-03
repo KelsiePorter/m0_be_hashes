019e237982e69b3a1f0d5742a0d7b11b23f0a34e
Kelsie Porter <krporter22@gmail.com>
2022-09-03 15:27:21 -0600

    Add part 1 and 2 of hashes 1 file

--- hashes_1.rb
+++ hashes_1.rb
@@ -1,29 +1,33 @@
 
-Activity:
+# Activity:
 
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your Terminal: 
+# file by entering the following command in your Terminal:
 # `ruby hashes_1.rb`
 
-foods = {"apples": 23, "grapes": 507, "eggs": 48}
-p zoo
+foods = {
+  apples: 23,
+  grapes: 507,
+  eggs: 48
+}
 
-# Write code that prints all of the 'keys' of the foods variable 
+# Write code that prints all of the 'keys' of the foods variable
 # you created above:
-# YOUR CODE HERE
+p foods.keys
 
-# Write code that prints all of the 'values' of the foods variable 
+# Write code that prints all of the 'values' of the foods variable
 # you created above:
-# YOUR CODE HERE
+p foods.values
 
-# Write code that prints the value of the second food of the foods variable 
+# Write code that prints the value of the second food of the foods variable
 # you created above:
-# YOUR CODE HERE
+p foods.keys[1]
 
-# Write code that adds a food to the foods hash. 
+# Write code that adds a food to the foods hash.
 # Then, print the updated hash:
-# YOUR CODE HERE
+foods["cheese"] = 2
+p foods
 
 
 #-------------------
@@ -39,14 +43,22 @@ p zoo
 
 # Write code that prints your email hash to the terminal.
 
+email_components = {
+  sender_name: "River Porter",
+  sender_email: "river@gmail.com",
+  time_stamp: "12:15",
+  subject: "Trip Itinerary",
+  body: "Attached is our trip initerary"
+}
 
-# Write code that prints all of the 'keys' of the email hash 
-# you created above:
-# YOUR CODE HERE
 
-# Write code that prints all of the 'values' of the email hash 
+# Write code that prints all of the 'keys' of the email hash
 # you created above:
-# YOUR CODE HERE
+p email_components.keys
+
+# Write code that prints all of the 'values' of the email hash
+# you created above:
+p email_components.values
 
 
 #-------------------
@@ -74,7 +86,7 @@ posts = [
     timestamp: "4:37 PM August 13, 2019",
     number_likes: 0,
     comments: []
-  }, 
+  },
   {
     image_src: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -88,12 +100,12 @@ p posts
 p posts[0]
 
 
-# The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK 
+# The code snippet above shows an Array with 2 elements. Each element in the Array is a
+# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK
 # a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 # good to have some exposure before going into Mod 1.
 
 
-# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same 
+# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same
 # key-value pairs you used in your email Hash above.
 # Then, print the email Array to the Terminal.
